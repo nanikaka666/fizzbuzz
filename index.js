e25c92a7fbbf9d608c18b7d263e379c9690cc297
nanikaka <nanikaka666@gmail.com>
2025-03-30 01:10:40 +0900

    fix correctly index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict'
 
 module.exports = function (num) {
-    if (num % 3 === 0) {
-        return 'Fizz'
+    if (num % 15 === 0) {
+        return 'FizzBuzz'
     } else if (num % 5 === 0) {
         return 'Buzz'
-    } else if (num % 15 === 0) {
-        return 'FizzBuzz'
+    } else if (num % 3 === 0) {
+        return 'Fizz'
     } else {
         return String(num)
     }
